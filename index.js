90596cede813af803833888de372b40a7220734e
Ryan Fink <rfink@redventures.net>
2012-10-25 13:34:34 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * Module-type namespace for the verdict decision engine.
+ * @author Ryan Fink <rfink@redventures.net>
+ * @since  September 3, 2011
+ */
+
+exports = module.exports verdict = {
+	comparison: require('lib/comparison'),
+	composite: require('lib/composite')
+};
